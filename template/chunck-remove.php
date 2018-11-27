3f0afd9bd14cdae21810cf2a08e3c2a93a5b0b3d
andygi <andy.greco@gekode.co.uk>
2018-11-27 12:14:55 +0000

    Update icons with Font Awsone v5 and starting function update with hook

--- template/chunck-remove.php
+++ template/chunck-remove.php
@@ -1,2 +1,2 @@
 <?php include('../lib/translate_tool.php'); ?>
-<i class="fa fa-trash-o"></i> <?php echo( __( 'remove My List')); ?>
+<i class="fas fa-heart"></i> <?php echo( __( 'remove My List')); ?>
--- template/chunk-view-whislist.php
+++ template/chunk-view-whislist.php
@@ -1,2 +1,2 @@
 <?php include('../lib/translate_tool.php'); ?>
-<i class="fa fa-list-ul"></i> <?php echo( __( 'view My List')); ?>
+<i class="fas fa-list-alt"></i> <?php echo( __( 'view My List')); ?>
